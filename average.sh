aab92db5db14c48ab6d0de21685a40fe4a3cd8e5
kobayashi9 <45445811+kobayashi9@users.noreply.github.com>
2019-05-28 20:44:11 +0900

    Initial commit

---
+++ average.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+goukei=0
+for i in `seq $2`
+do
+	array+=(`\time -f %e python3 ./${1} 2>&1`)
+done
+
+for i in ${array[@]}
+do
+	goukei=`echo "scale=5; $goukei+$i" | bc`
+done
+echo "scale=5; $goukei / $2" | bc >> result.txt  
